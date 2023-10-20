79906632a236ea419e8ec0caa7832e8d722db6c5
이나령 <thisisme20@daum.net>
2023-10-20 12:08:38 +0900

    section5: Javascript Proxy 이해

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -17,3 +17,33 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+
+/**
+ * [Javascript Proxy에 대해 알아보자]
+ * 개요: Javascript는 기본적으로 반응형이 아니다.
+ */
+const data = {
+  message: 'Hello!',
+  longMessage: "I am brave, I am bruised. I am who I'm meant to be, ",
+};
+
+// setter 함수를 정의해서 proxy에 새 프로퍼티가 설정될 때마다 트리거됨.
+const handler = {
+  set(target, key, value) {
+    if (key === 'message') {
+      target.longMessage = value + 'this is me'
+    }
+    target.message = value;
+
+    console.log(target);
+    console.log(key);
+    console.log(value);
+  }
+}
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = 'HELLO!';
+
+console.log(proxy.longMessage);
